733b5d9e49c6389a22d8f69fd68a77fcfb0ad10c
vishal <subhavishal@yahoo.co.in>
2022-06-14 18:44:46 +0530

    added some comments

--- NO_simulation/NO_rxn.m
+++ NO_simulation/NO_rxn.m
@@ -6,4 +6,18 @@ setGlobalvars();
 % Species  1:NO, 2:CO, 3:NO.S, 4:N.S, 5:O.S, 6:N2, 7:CO.S, 8:CO2.S, 9:N2O.S
 % 10:N2O, 11:CO2
 initial = zeros(10,1);
-
+% Strategy:
+% 1. For given feed flow rates find concentrations: ci = Fi/sum(F)*Ctotal.
+% Ctotal = Preactor/RTreactor
+% 2. dFi/dV = rate of change of concentration of that species
+% 3. For a given volume simultaneously solve the above set of odes. i.e. 
+% write a function that performs step 1 and 2 and feed it to ode45 imposing
+% limits on the volume.
+%% RR method
+% Strategy:
+% Unlike the microkinetic model, only rates that are of concern to us are
+% those of the terminal species (initial reactants and final products).
+% Essentially follow the same procedure, except pretend that you have only 
+% the terminal species whose rates are given by the RR method eqns overall
+% rate. some reactants/products might have rOR1 + rOR2 as their rates since
+% this is a parallel reaction.
--- NO_simulation/mk_deqns.m
+++ NO_simulation/mk_deqns.m
@@ -1,7 +1,22 @@
 function d = mk_deqns(c,T)
     % Species  1:NO, 2:CO, 3:NO.S, 4:N.S, 5:O.S, 6:N2, 7:CO.S, 8:CO2.S, 9:N2O.S
     % 10:N2O, 11:CO2
+    k_f = pre_exp_f*exp(-1*E_forward/(R*T))
+    k_b = pre_exp_b*exp(-1*E_back/(R*T))
+    r = rates(c,k_f,k_b);
     
-    r = zeros(8,1);
-    r(1) = 
+end
+function rk = rates(c,k_f,k_b)
+    rk = zeros(8,1);
+    % Species  1:NO, 2:CO, 3:NO.S, 4:N.S, 5:O.S, 6:N2, 7:CO.S, 8:CO2.S, 9:N2O.S
+    % 10:N2O, 11:CO2
+    % equations incomplete but simple differential eqns
+    rk(1) = kf(1)*c(1) - k_b(1)*c(3);
+    rk(2) = kf(2) - k_b(2);
+    rk(3) = kf(3) - k_b(3);
+    rk(4) = kf(4) - k_b(4);
+    rk(5) = kf(5) - k_b(5);
+    rk(6) = kf(6) - k_b(6);
+    rk(7) = kf(7) - k_b(7);
+    rk(8) = kf(8) - k_b(8);
 end
